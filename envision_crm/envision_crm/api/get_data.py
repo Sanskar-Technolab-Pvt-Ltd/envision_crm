7b9192a149b16897dde4dc22fb9e14d1fcbdd7ee
Yogesh <yogesh@sanskartechnolab.com>
2024-08-13 12:15:58 +0530

    fix the error the in the auto fetch data in the print offer section

--- envision_crm/envision_crm/api/get_data.py
+++ envision_crm/envision_crm/api/get_data.py
@@ -6,6 +6,6 @@ def get_print_template_details(template):
     template_details = frappe.get_doc("Quotation Print Templates", template)
     print(template_details)
     return {
-        "template_heading": template_details.heading,
+        "template_heading": template_details.heading_name,
         "template_description": template_details.content,
     }
