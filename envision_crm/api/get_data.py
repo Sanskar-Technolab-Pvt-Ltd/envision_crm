03f2267c207686c4562b60aa29d499a7b0abfd33
Yogesh <yogesh@sanskartechnolab.com>
2024-08-20 11:39:02 +0530

    remove customization of quotation item table

--- envision_crm/api/get_data.py
+++ envision_crm/api/get_data.py
@@ -4,7 +4,6 @@ import frappe
 @frappe.whitelist()
 def get_print_template_details(template):
     template_details = frappe.get_doc("Quotation Print Templates", template)
-    print(template_details)
     return {
         "template_heading": template_details.heading_name,
         "template_description": template_details.content,
